796a6d023cdc9fcc7c9d790863c55456502c27a0
cberleux <clement.berleux@gmail.com>
2024-11-18 15:50:44 +0100

    Installation de Tailwind CSS et Preline UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Event, Router, RouterOutlet, NavigationEnd } from '@angular/router';
+
+import { IStaticMethods } from 'preline/preline';
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -10,4 +17,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Todo';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          window.HSStaticMethods.autoInit();
+        }, 100);
+      }
+    });
+  }
 }
